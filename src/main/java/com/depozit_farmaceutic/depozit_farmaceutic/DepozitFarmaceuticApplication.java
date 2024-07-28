fc56efbf2addefc485ab3b9562b26e5ae3fbbcfd
catacoman1 <catalin.coman@student.upt.ro>
2024-07-28 18:19:29 +0300

    first commit

---
+++ src/main/java/com/depozit_farmaceutic/depozit_farmaceutic/DepozitFarmaceuticApplication.java
@@ -0,0 +1,13 @@
+package com.depozit_farmaceutic.depozit_farmaceutic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepozitFarmaceuticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepozitFarmaceuticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/depozit_farmaceutic/depozit_farmaceutic/DepozitFarmaceuticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depozit_farmaceutic.depozit_farmaceutic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepozitFarmaceuticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
